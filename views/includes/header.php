95bd83dcd26ce983ef7e5bcac00428cebcc5ce4e
Kauan Felipe <67879283+KauanKNFE@users.noreply.github.com>
2024-11-16 01:49:56 -0300

    Add files via upload

--- views/includes/header.php
+++ views/includes/header.php
@@ -4,10 +4,10 @@
   <div class="topo pure-g">
     <div class="pure-u-1 pure-u-md-1-3">
       <div class="header-left">
-        <a href="../Faq"><img src="../../img/Faq.png" alt="Ícone FAQ"></a>
-        <a href="www.instagram.com/vamgarkacademy" class="icon-link"><i class="fab fa-instagram"></i></a> 
-        <a href="#" class="icon-link"><i class="fab fa-facebook"></i></a>
-        <a href="#" class="icon-link"><i class="fab fa-twitter"></i></a>
+        <a href="../Faq" class="social"><img src="../../img/Faq.png" alt="Ícone FAQ"></a>
+        <a href="www.instagram.com/vamgarkacademy" class="icon-link social"><i class="fab fa-instagram"></i></a> 
+        <a href="#" class="icon-link social"><i class="fab fa-facebook"></i></a>
+        <a href="#" class="icon-link social"><i class="fab fa-twitter"></i></a>
       </div>
     </div>
     <div class="pure-u-1 pure-u-md-1-3">
@@ -18,7 +18,7 @@
     <div class="pure-u-1 pure-u-md-1-3">
       <div class="header-right">
         <div class="search">
-          <input type="text" placeholder="Pesquisar">
+          <input type="text" placeholder="Buscar">
           <i class="fas fa-search"></i>
         </div>
         <div class="minhaconta">
@@ -26,24 +26,44 @@
         </div>
       </div>
     </div>
+     <a href="#" class="menu-toggle"> <i class="fas fa-bars"></i> </a> 
   </div>
+
   <div class="menu">
     <ul class="nav-menu pure-g">
+      
       <li class="pure-u-1-6"><a href="../Home">Página Inicial</a></li>
       <li class="pure-u-1-6"><a href="../Sobre">Sobre o Curso</a></li>
       <li class="pure-u-1-6"><a href="../Eventos">Eventos</a></li>
       <li class="pure-u-1-6"><a href="../Forum">Fórum</a></li>
       <li class="pure-u-1-6"><a href="../Mercado">Mercado</a></li>
       <li class="pure-u-1-6"><a href="../Casa">Sua Casa</a></li>
+      <li class="pure-u-1-6 minhaconta"><a href="../MinhaConta" class="button">Minha Conta</a></li>
+      <div class="socials">
+      <li class="social"><a href="../Faq"><img src="../../img/Faq.png" class="faq" alt="Ícone FAQ"></a></li>
+      <li class="social"><a href="www.instagram.com/vamgarkacademy" class="icon-link"><i class="fab fa-instagram"></i></a></li>
+      <li class="social"><a href="#" class="icon-link"><i class="fab fa-facebook"></i></a></li>
+      <li class="social"><a href="#" class="icon-link"><i class="fab fa-twitter"></i></a></li>
+      </div>
     </ul>
   </div>
+
   <div class="page-indicator">
     <span>Você está aqui</span>
   </div>
 
   <style>
     .page-indicator{
-        transform: translateX(calc(174% * (<?php echo $pagina_atual - 1; ?>)));
+      transform: translateX(calc(14.28571428571429% * (<?php echo $pagina_atual - 1; ?>)));
     }
   </style>
 </header>
+
+<script>
+  const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+</script>
